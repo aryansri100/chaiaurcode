e7515d1050be3cbdef0f8c62edc397a9943aac82
aryansri100 <137744345+aryansri100@users.noreply.github.com>
2026-02-05 15:15:39 +0000

    second day

--- .devcontainer/basic.js
+++ .devcontainer/basic.js
@@ -1,1 +1,7 @@
-console.log('Aryan')
+const account_Id=1222345
+let account_email="ac@ac.com"
+var account_name="aryan"
+account_city="allahabad"
+let account;
+/*Prefer to use let instead of var */
+console.table([account_Id,account_email,account_name,account_city,account]);
---
+++ .devcontainer/lec2.js
@@ -0,0 +1,3 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3) does not work as we are using node js and not browser
