c19f6f34cd1fda14e9dff4a7bb0f168e1576ff1c
aryansri100 <137744345+aryansri100@users.noreply.github.com>
2026-02-19 13:22:41 +0000

    day 3

---
+++ .devcontainer/stringbasic.js
@@ -0,0 +1,7 @@
+const name="Aryan";
+const repoCount=50;
+//console.log(name+50+" value"); -> this works but is outdated now
+
+//console.log(`Hello my name is ${name} and my repocount is ${repoCount}`)-> this is the new and recommended version
+
+//const gameName=new String('Aryan')->recomended for declaring string
